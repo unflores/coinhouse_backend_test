bd27401d083893a8b0bebd1a8230884bed575188
cmiran <cmiran@student.42.fr>
2021-03-12 00:26:51 +0100

    create user, again all the validations i can think about

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+  has_many :events
+  has_many :speeches, class_name: 'Event', foreign_key: :speaker_id
+
+  has_and_belongs_to_many :registrations, class_name: 'Event'
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates_presence_of :first_name, :last_name
+end
