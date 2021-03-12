c7f150194e4827d1a53419183021f9f3b7c1da93
cmiran <cmiran@student.42.fr>
2021-03-12 21:46:03 +0100

    for later

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,14 @@ module EventApi
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'UTC'
+    # config.active_record.default_timezone = :utc
+    # config.active_record.schema_format = :sql
+
+    # config.eager_load_paths << Rails.root.join("extras")
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#todo make seeding works
+# If in Spring & test env, load spec_helper.rb
+# if ENV['SPRING_ENV'] == 'test' && ENV['RAILS_ENV'] == 'test'
+#   require File.expand_path('../../spec/spec_helper', __FILE__)
+# end
