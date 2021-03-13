6844ca6924f73a48283750f9e1611adf9013e8e4
cmiran <cmiran@student.42.fr>
2021-03-13 20:30:14 +0100

    modify seeds, add scopes on the relations of user with events (workshop/office_hours)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
 
-  has_many :events
-  has_many :speeches, class_name: 'Event', foreign_key: :speaker_id
+  has_many :events do
+    def workshops; where(kind: 0) end
+    def office_hours; where(kind: 1) end
+  end
+  has_many :speeches, class_name: 'Event', foreign_key: :speaker_id do
+    def workshops; where(kind: 0) end
+    def office_hours; where(kind: 1) end
+  end
 
-  has_and_belongs_to_many :registrations, class_name: 'Event'
+  has_and_belongs_to_many :registrations, class_name: 'Event' do
+    def workshops; where(kind: 0) end
+    def office_hours; where(kind: 1) end
+  end
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
-ActiveRecord::Base.transaction {
+User.destroy_all
+Event.destroy_all
 
-  25.times do
-    user_params = {
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      email: Faker::Internet.unique.email
-    }
+25.times do
+  user_params = {
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.unique.email
+  }
 
-    User.create(user_params)
-  end
+  User.create(user_params)
+end
 
-  10.times do
-    kind = rand(0..1)
-    date = Faker::Time.forward(days: 1, period: :morning)
-    event_params = {
-      user_id: User.pluck(:id).sample,
-      speaker_id: User.pluck(:id).sample,
-      kind: kind,
-      date: date,
-      start_at: Faker::Time.between_dates(from: date, to: date + 1.second),
-      end_at: Faker::Time.between_dates(from: date + 1.second, to: date.end_of_day),
-      name: Faker::CryptoCoin.coin_name,
-      location: Faker::WorldCup.stadium,
-      description: Faker::Lorem.paragraph,
-    }
-    event_params[:limit] = rand(1..5) if kind == 0
+10.times do
+  kind = rand(0..1)
+  date = Faker::Time.forward(days: 1, period: :morning)
+  event_params = {
+    user_id: User.pluck(:id).sample,
+    speaker_id: User.pluck(:id).sample,
+    kind: kind,
+    date: date,
+    start_at: Faker::Time.between_dates(from: date, to: date + 1.second),
+    end_at: Faker::Time.between_dates(from: date + 1.second, to: date.end_of_day),
+    name: Faker::CryptoCoin.coin_name,
+    location: Faker::WorldCup.stadium,
+    description: Faker::Lorem.paragraph,
+  }
+  event_params[:limit] = rand(1..5) if kind == 0
 
-    Event.create(event_params)
-  end
-}
+  Event.create(event_params)
+end
